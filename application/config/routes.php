813714c3843f2076dc1e42321f10f92f56a3080a
Madhusudhan <madhusudhan.webomindapps.com>
2020-03-05 10:17:08 +0530

    changes to router

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,13 +1,11 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-// $route['localhost/webo-cms/']                  = 'localhost/webo_cms/';
-$route['client-master']                        = 'Client_master';
-$route['client-master/save-client-master']     = 'Client_master/save_client_master';
-$route['ssl-remainder']     = 'Ssl_remainder';
-$route['ssl-remainder/dispaly-relavent-websites']     = 'Ssl_remainder/dispaly_relavent_websites';
 
-$route['master-remainder']  = 'Master_remainder';
-
-$route['default_controller'] = 'webo_home';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+$route['client-master']                           = 'Client_master';
+$route['client-master/save-client-master']        = 'Client_master/save_client_master';
+$route['ssl-remainder']                           = 'Ssl_remainder';
+$route['ssl-remainder/dispaly-relavent-websites'] = 'Ssl_remainder/dispaly_relavent_websites';  
+$route['master-remainder']                        = 'Master_remainder'; 
+$route['default_controller']                      = 'webo_home';
+$route['404_override']                            = '';
+$route['translate_uri_dashes']                    = FALSE;
