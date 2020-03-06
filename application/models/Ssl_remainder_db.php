2370bdcbde9afccc2d3b38abed81dfc7ad0f8b7f
Madhusudhan <madhusudhan.webomindapps.com>
2020-03-06 11:34:39 +0530

    changes to ssi remainder by madhu

--- application/models/Ssl_remainder_db.php
+++ application/models/Ssl_remainder_db.php
@@ -31,5 +31,59 @@ class Ssl_remainder_db extends CI_Model
         } else {
             return false;
         }
-    } 
+    }
+
+    function insert_ssl_remainder_db()
+    {
+        $company_name_selected = $this->input->post('company_name_selected');
+        $this->db->select('id');
+        $this->db->where('company_name', $company_name_selected);
+        $query = $this->db->get('client_master');
+        $company_id = $query->result_array();
+        $company_id = $company_id[0]['id'];
+        // echo "<pre>";
+        // echo $company_id;
+        // exit; 
+        $company_website_selected = $this->input->post('company_website_selected');
+        $renewel_method_selected = $this->input->post('renewel_method_selected');
+        $manual_update_date = "";
+        $renewel_date = "";
+        if (!empty($_POST['manual_update_date'])) {
+            $manual_update_date = $this->input->post('manual_update_date');
+            $renewel_date = $this->input->post('manual_renewel_date');
+            // echo "<pre>";
+            // echo $renewel_date;
+            // echo $manual_update_date;
+            // exit;
+        } else if (!empty($_POST['auto_renewel_date'])) {
+            $renewel_date = $this->input->post('auto_renewel_date');
+        }
+        $amount_selected = $this->input->post('amount_selected');
+
+        $field = array('company_id' => $company_id, 'company_website' => $company_website_selected, 'type' => $renewel_method_selected, 'manual_update_date' => $manual_update_date, 'renewel_date' => $renewel_date, 'amount_paid' => $amount_selected);
+        if ($this->db->insert("add_ssl_remainder", $field)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function del_new_user()
+    {
+        $id  = $this->input->post('del_id');
+        $this->db->where('id', $id);
+        $this->db->delete("user_master");
+    }
+    function update_new_user_record()
+    {
+        $name = $this->input->post('doctor_username');
+        $password = $this->input->post('doctor_password');
+        $password = hash('sha512', $password);
+        $id = $this->input->post('update_new_created_user_id');
+        $field = array('user_name' => $name, '	user_password' => $password);
+        $this->db->where('id', $id);
+        if ($this->db->update('user_master', $field)) {
+            return true;
+        }
+    }
 }
---
+++ application/views/ssl_remainder/index_copy.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>WeboCMS</title>
+
+    <!-- Global stylesheets -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url(); ?>admin_assets/global_assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url(); ?>admin_assets/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url(); ?>admin_assets/assets/css/bootstrap_limitless.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url(); ?>admin_assets/assets/css/layout.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url(); ?>admin_assets/assets/css/components.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url(); ?>admin_assets/assets/css/colors.min.css" rel="stylesheet" type="text/css">
+    <!-- /global stylesheets -->
+
+    <!-- Core JS files -->
+    <script src="<?php echo base_url(); ?>admin_assets/global_assets/js/main/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>admin_assets/global_assets/js/main/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url(); ?>admin_assets/global_assets/js/plugins/loaders/blockui.min.js"></script>
+    <!-- /core JS files -->
+
+    <!-- Theme JS files -->
+
+    <script src="<?php echo base_url(); ?>admin_assets/global_assets/js/plugins/tables/datatables/datatables.min.js"></script>
+    <script src="<?php echo base_url(); ?>admin_assets/global_assets/js/plugins/tables/datatables/extensions/responsive.min.js"></script>
+    <script src="<?php echo base_url(); ?>admin_assets/global_assets/js/plugins/forms/selects/select2.min.js"></script>
+
+    <script src="<?php echo base_url(); ?>admin_assets/assets/js/app.js"></script>
+    <script src="<?php echo base_url(); ?>admin_assets/global_assets/js/demo_pages/datatables_responsive.js"></script>
+    <!-- /theme JS files -->
+    <!-- fafa-font -->
+    <script src="https://kit.fontawesome.com/f64c26b0b8.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxy/1.6.1/scripts/jquery.ajaxy.min.js" crossorigin="anonymous"></script>
+    <style>
+        .table_font {
+            font-size: 14px;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <!-- Main navbar ------------------------------------------------------------------------------------------------------>
+    <?php
+    $this->load->view('includes/main_navbar');
+    ?>
+    <!-- /main navbar -->
+
+
+    <!-- Page content -->
+    <div class="page-content">
+
+        <!-- Main sidebar -------------------------------------------------------------------------------------------------------->
+        <?php
+        $this->load->view('includes/main_sidebar');
+        ?>
+        <!-- /main sidebar -->
+
+        <!-- Main content -->
+        <div class="content-wrapper">
+
+            <!-- Page header -->
+            <div class="page-header page-header-light">
+                <div class="page-header-content header-elements-md-inline">
+                    <div class="page-title d-flex">
+                        <h4><i style="font-size:18px;" class="fa fa-lock"></i> &nbsp;SSL Remainder</h4>
+                    </div>
+                </div>
+            </div>
+            <!-- /page header -->
+
+
+            <!-- Content area -->
+
+            <div class="card col-md-6">
+                <div class="card-body">
+                    <form action="#" method="post">
+
+
+                        <div class="form-group form-group-feedback form-group-feedback-left" bis_skin_checked="1">
+                            <select class="form-control" id="call_relavent_websites" required>
+                                <option value="">Select Company</option>
+                                <?php
+                                for ($i = 0; $i < count($company_names_db); $i++) :
+                                    echo '<option  value="' . $company_names_db[$i]['company_name'] . '">' . $company_names_db[$i]['company_name'] . '</option>';
+                                endfor;
+                                ?>
+                            </select>
+                            <div class="form-control-feedback" bis_skin_checked="1">
+                                &nbsp;&nbsp; <i class="fas fa-building"></i>
+                            </div>
+                        </div>
+
+                        <div class="form-group form-group-feedback form-group-feedback-left" bis_skin_checked="1">
+                            <select class="form-control" id="dynamic_company_websites" required>
+                                <option value="">Select Website</option>
+                            </select>
+                            <div class="form-control-feedback" bis_skin_checked="1">
+                                &nbsp;&nbsp; <i class="fa fa-globe" aria-hidden="true"></i>
+                            </div>
+                        </div>
+
+                        <!-- //------------------------------------------------------------------------ -->
+
+
+                        <div class="card-header header-elements-inline">
+                            <h6 class="card-title">Select Renew Method</h6>
+                            <div class="header-elements">
+                                <div class="list-icons">
+                                    <a class="list-icons-item" data-action="collapse"></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div id="alpaca-radio-basic">
+                                <div class="form-group alpaca-field alpaca-field-radio alpaca-required alpaca-edit alpaca-top alpaca-field-valid" data-alpaca-field-id="alpaca7" data-alpaca-field-path="/" data-alpaca-field-name="renew_method">
+
+                                    <div class="radio alpaca-control form-check" name="renew_method" style="display: block;">
+                                        <label class="form-check-label">
+                                            <input id="renew_radio_manual" type="radio" name="renew_method" value="manual" class="form-check-input" required>Manual
+                                        </label>
+                                    </div>
+
+                                    <div style="margin:10px;display:none" id="renew_buttons_manual">
+                                        <button type="button" style="margin:0 15px 0 22px;" class="btn bg-teal-400 btn-labeled btn-labeled-left"><b><i class="fas fa-upload"></i></b>New Update</button>
+
+                                        <button type="button" class="btn bg-teal-400 btn-labeled btn-labeled-left"><b><i class="fas fa-sync"></i></b>Renew</button>
+                                    </div>
+
+                                    <div class="radio alpaca-control form-check" name="renew_method" style="display: block;">
+                                        <label class="form-check-label">
+                                            <input id="renew_radio_auto" type="radio" name="renew_method" value="auto" class="form-check-input" required>Auto
+                                        </label>
+                                    </div>
+
+                                    <div style="margin:10px;display:none" id="renew_buttons_auto">
+                                        <button type="button" style="margin:0 15px 0 22px;" class="btn bg-teal-400 btn-labeled btn-labeled-left"><b><i class="fas fa-sync"></i></b>Renew</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn-success">Save</button>
+
+
+                    </form>
+                </div>
+                <!-- /content area -->
+
+            </div>
+            <!-- /main content -->
+
+
+        </div>
+        <!-- /page content -->
+
+
+        <script>
+            //hide options in radio
+            $(document).ready(function() {
+
+                $("#renew_radio_manual").click(function() {
+                    var radio_manual = $(this).val();
+
+                    $("#renew_buttons_manual").css('display', 'block');
+                    $("#renew_buttons_auto").css('display', 'none');
+                });
+                $("#renew_radio_auto").click(function() {
+                    var radio_auto = $(this).val();
+                    $("#renew_buttons_auto").css('display', 'block');
+                    $("#renew_buttons_manual").css('display', 'none');
+                });
+                //calling relevant websites based on client name
+                $('#call_relavent_websites').change(function() {
+
+                    var company_name = $('#call_relavent_websites').val();
+                    if (company_name) {
+                        jQuery.ajax({
+                            type: 'POST',
+                            url: "<?php echo base_url(); ?>" + "ssl-remainder/dispaly-relavent-websites",
+                            data: {
+                                company_name: company_name
+                            },
+                            success: function(response) {
+                                $("#dynamic_company_websites").empty();
+                                $("#dynamic_company_websites").append(response);
+                            }
+                        });
+                    }
+                });
+
+                // $('#alpaca-radio-styled').alpaca({
+                //     data: 'Jimi Hendrix',
+                //     schema: {
+                //         enum: ['Jimi Hendrix', 'Mark Knopfler', 'Joe Satriani', 'Eddie Van Halen', 'Orianthi']
+                //     },
+                //     options: {
+                //         type: 'radio',
+                //         label: 'Who is your favorite guitarist?',
+                //         fieldClass: 'radio-styled-demo',
+                //         vertical: true
+                //     },
+                //     postRender: function(control) {
+                //         $('.radio-styled-demo').find('input[type=radio]').uniform({
+                //             radioClass: 'choice'
+                //         });
+                //     }
+                // });
+            });
+        </script>
+</body>
+
+</html>
