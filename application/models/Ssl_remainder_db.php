6aa4f23d8e502c61f97e74b4c037d2237c293557
Madhusudhan <madhusudhan.webomindapps.com>
2020-03-06 12:42:30 +0530

    changes to sslremainder by madhu

--- application/models/Ssl_remainder_db.php
+++ application/models/Ssl_remainder_db.php
@@ -7,6 +7,59 @@ class Ssl_remainder_db extends CI_Model
     {
         parent::__construct();
     }
+    //Data tables code
+
+    public function make_query()
+    {
+        $order_column = array("a.id", "c.company_name", "a.company_website", "a.type", "a.manual_update_date", "a.renewel_date", "a.amount_paid");
+        $this->db->select('a.*,c.company_name');
+        $this->db->from('add_ssl_remainder a');
+        $this->db->join('client_master c', 'a.company_id=c.id', 'left');
+        if (isset($_POST["search"]["value"])) {
+            $this->db->group_start();
+            $this->db->like("a.id", $_POST["search"]["value"]);
+            $this->db->or_like("c.company_name", $_POST["search"]["value"]);
+            $this->db->or_like("a.company_website", $_POST["search"]["value"]);
+            $this->db->or_like("a.type", $_POST["search"]["value"]);
+            $this->db->or_like("a.manual_update_date", $_POST["search"]["value"]);
+            $this->db->or_like("a.renewel_date", $_POST["search"]["value"]);
+            $this->db->or_like("a.amount_paid", $_POST["search"]["value"]);
+            $this->db->group_end();
+        }
+        if (isset($_POST["order"])) {
+            $this->db->order_by($order_column[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+        } else {
+            $this->db->order_by('a.id', 'DESC');
+        }
+    }
+
+    function get_all_data()
+    {
+        $this->db->select('a.*,c.company_name');
+        $this->db->from('add_ssl_remainder a');
+        $this->db->join('client_master c', 'a.company_id=c.id', 'left');
+        // $this->db->select("*");
+        // $this->db->from('add_ssl_remainder');
+        return $this->db->count_all_results();
+    }
+
+    function get_filtered_data()
+    {
+        $this->make_query();
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+
+    function make_datatables()
+    {
+        $this->make_query();
+        if ($_POST["length"] != -1) {
+            $this->db->limit($_POST['length'], $_POST['start']);
+        }
+        $query = $this->db->get();
+        return $query->result();
+    }
+    //Data tables code
     public function fetch_company_names()
     {
         $this->db->select('company_name');
